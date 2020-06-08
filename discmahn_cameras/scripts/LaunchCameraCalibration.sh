deeb78c1e62ea7e9d8d2ad3b949243d42d4dd219
TJLW <tjlwhitaker@gmail.com>
2020-06-08 17:39:04 -0500

    Playing with QR codes

--- discmahn_cameras/scripts/LaunchCameraCalibration.sh
+++ discmahn_cameras/scripts/LaunchCameraCalibration.sh
@@ -1,1 +1,2 @@
-rosrun camera_calibration cameracalibrator.py --size 10x7 --square 0.020 image:=/webcam/image_raw camera:=/webcam
+#rosrun camera_calibration cameracalibrator.py --size 10x7 --square 0.020 image:=/webcam/image_raw camera:=/webcam
+rosrun camera_calibration cameracalibrator.py --size 8x6 --square 0.070 image:=/webcam/image_raw camera:=/webcam
